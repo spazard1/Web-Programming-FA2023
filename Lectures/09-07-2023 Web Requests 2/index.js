781062cb7772585608236eb03e823eb1c04bc30e
Steven Yackel <spazard1@gmail.com>
2023-09-07 19:04:26 -0500

    adds web requests 2 lecture

--- Lectures/09-07-2023 Web Requests 2/index.js
+++ Lectures/09-07-2023 Web Requests 2/index.js
@@ -23,14 +23,45 @@ How we talk to the server:
 2. Body/Payload, the data we want to create/read/update/delete
 3. Headers, metadata of the request
 	Content-Type, the type of the body/payload
-
+	Content-Length, the size in bytes of the body/payload
+	Accept, the type that I want to receive back from the server
+	User-Agent, the type of browser that I am using
+	Authorization, your user token to authenticate to the server
 
 How the server talks back to us:
-1. Status Code
-	200 - Success
-	404 - NotFound
+1. Status Codes
 2. Body/Payload, the data that we asked for or created
 3. Headers, the metdata of the response
+	Content-Type, the type of the body/payload
+	Content-Length, the size in bytes of the body/payload
+	Location, used by redirect requests (3xx status codes)
+	Date, the date/time of the response
+
+
+Status Codes:
+
+XYY - X is the type of status code, and Y is the version of that status code
+
+100
+
+200 - OK
+	201 - Created, usually from a POST request
+	202 - Accepted, work is not done yet, but has begun processing
+	204 - No Content, usually from a DELETE request
+
+300 - Redirect "the content is somewhere else"
+	301 - Moved Permanently, "never come back here, the content will always be at the new place"
+	307 - Temporary Redirect, "come back here later, the content will be back here"
+
+400 - BadRequest "it's YOUR fault"
+	401 - Unauthorized, "you haven't logged in yet"
+	403 - Forbidden, "you are logged in, but you can't do that"
+	404 - Not Found, "the content you are looking for doesn't exist"
+	429 - Too Many Requests, "you are doing that too much, slow down"
+
+500 - InternalServerError "it's the servers fault"
+	Likely the server threw an exception
+	503 - Service unavailable "the server is busy right for any number of reasons"
 
 */
 
