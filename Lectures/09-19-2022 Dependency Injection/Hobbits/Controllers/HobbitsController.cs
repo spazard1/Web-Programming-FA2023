26b98cd21e82c822017784ba180c56eec3064a58
Steven Yackel <spazard1@gmail.com>
2023-09-19 12:34:29 -0500

    base dependency injection lecture

---
+++ Lectures/09-19-2022 Dependency Injection/Hobbits/Controllers/HobbitsController.cs
@@ -0,0 +1,46 @@
+﻿using Hobbits.Entities;
+using Hobbits.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Hobbits.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HobbitsController : Controller
+    {
+
+        private readonly static HobbitsDatabase hobbitsDatabase = new();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json(hobbitsDatabase.GetAll().Select(hobbit => new HobbitEntity(hobbit)));
+        }
+
+        [HttpGet("{index:int}")]
+        public IActionResult Get(int index)
+        {
+            if (index < 0 || index >= hobbitsDatabase.Count)
+            {
+                return StatusCode((int)HttpStatusCode.NotFound);
+            }
+
+            return Json(new HobbitEntity(hobbitsDatabase.Get(index)));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] HobbitEntity hobbitEntity)
+        {
+            hobbitsDatabase.Add(hobbitEntity.ToModel());
+
+            return Json(hobbitEntity);
+        }
+    }
+}
---
+++ Lectures/09-19-2022 Dependency Injection/Hobbits/Services/HobbitsDatabase.cs
@@ -0,0 +1,39 @@
+﻿using Hobbits.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hobbits.Services
+{
+    public class HobbitsDatabase
+    {
+        private readonly List<HobbitModel> hobbits = new List<HobbitModel>()
+        {
+            new HobbitModel() { Name = "Frodo", Group = "hobbits" },
+            new HobbitModel() { Name = "Sam", Group = "hobbits" },
+            new HobbitModel() { Name = "Merry", Group = "hobbits" },
+            new HobbitModel() { Name = "Pippin", Group = "hobbits" }
+        };
+
+        public int Count
+        {
+            get => hobbits.Count;
+        }
+
+        public IEnumerable<HobbitModel> GetAll()
+        {
+            return hobbits;
+        }
+
+        public HobbitModel Get(int id)
+        {
+            return hobbits[id];
+        }
+
+        public void Add(HobbitModel hobbitModel)
+        {
+            hobbits.Add(hobbitModel);
+        }
+    }
+}
