a6fb07d40882d23463d264dccf103d3ddc17201f
Steven Yackel <spazard1@gmail.com>
2023-09-28 14:55:32 -0500

    TFH lecture

--- Lectures/09-28-2023 Transient Fault Handling/Mario/Mario/Services/ExternalMarioService.cs
+++ Lectures/09-28-2023 Transient Fault Handling/Mario/Mario/Services/ExternalMarioService.cs
@@ -1,5 +1,8 @@
 ﻿using Mario.Entities;
 using Newtonsoft.Json;
+using Polly;
+using System.Diagnostics;
+using System.Net;
 
 namespace Mario.Services
 {
@@ -9,8 +12,29 @@ namespace Mario.Services
 
         public async Task<ExternalMarioEntity?> GetMarioLevelResultAsync()
         {
-            var response = await httpClient.GetAsync("https://www.google.com");
-            var responseString = await response.Content.ReadAsStringAsync();
+            var policy = Policy.HandleInner<HttpRequestException>((ex) =>
+            {
+                return ex?.StatusCode == HttpStatusCode.ServiceUnavailable;
+            }).WaitAndRetryAsync(5, retryAttempt =>
+                TimeSpan.FromMilliseconds(100 * Math.Pow(2, retryAttempt))
+            ,(ex, timeSpan, context) =>
+            {
+                Debug.WriteLine("We are trying again! " + ex.Message);
+            });
+
+            string? responseString = null;
+
+            await policy.ExecuteAsync(async () =>
+            {
+                var response = await httpClient.GetAsync("https://webprogrammingmario.azurewebsites.net/api/mario/jump");
+                response.EnsureSuccessStatusCode();
+                responseString = await response.Content.ReadAsStringAsync();
+            });
+
+            if (responseString == null)
+            {
+                return null;
+            }
 
             return JsonConvert.DeserializeObject<ExternalMarioEntity?>(responseString);
         }
