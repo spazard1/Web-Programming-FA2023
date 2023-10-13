e90422c3a17e350c026247f68efe646ff8f7c2d6
Steven Yackel <spazard1@gmail.com>
2023-10-12 20:33:39 -0500

    queues 2

---
+++ Lectures/10-12-2023 Queues 2/Kuscotopia/Kuscotopia/Services/QueueService.cs
@@ -0,0 +1,33 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Common;
+using Common.Entities;
+using Newtonsoft.Json;
+
+namespace Kuscotopia.Services
+{
+    public class QueueService
+    {
+        private static BasicAWSCredentials credentials;
+        private static AmazonSQSClient sqsClient;
+
+        public QueueService()
+        {
+            credentials = new BasicAWSCredentials(Credentials.QueueKeyId, Credentials.QueueKey);
+            sqsClient = new AmazonSQSClient(credentials, RegionEndpoint.USEast1);
+        }
+
+        public async Task QueueWorkAsync(WorkEntity workEntity)
+        {
+            var sendMessageRequest = new SendMessageRequest()
+            {
+                QueueUrl = Credentials.QueueUrl,
+                MessageBody = JsonConvert.SerializeObject(workEntity)
+            };
+
+            await sqsClient.SendMessageAsync(sendMessageRequest);
+        }
+    }
+}
